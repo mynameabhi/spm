fbd7a425c22a94736aaf946b6aeb0b7c84155d9f
mynameabhi <75668852+mynameabhi@users.noreply.github.com>
2022-04-14 20:36:39 +0530

    Add files via upload

---
+++ emoji_script.py
@@ -0,0 +1,65 @@
+# from tracemalloc import start
+from tracemalloc import start
+import pyautogui
+import time
+
+time.sleep(2)
+
+pyautogui.write("strt.")
+pyautogui.press("enter")
+
+# ------here spam start---------
+
+pyautogui.hotkey("ctrl" , "v"  )
+pyautogui.write(" $")
+pyautogui.press("enter")
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" %")
+pyautogui.press("enter")
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" &")
+pyautogui.press("enter")
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" ~")
+pyautogui.press("enter")
+
+
+# 💔 💓 💕 💛 ~~~~heart emoji~~~~💌 💗 💝 💛 💜
+pyautogui.write(" emoji start", interval="0")
+pyautogui.press("enter")
+
+# start here
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" strt.")
+pyautogui.press("enter")
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" /")
+pyautogui.press("enter")
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" |")
+pyautogui.press("enter")
+
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" #")
+pyautogui.press("enter")
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" %")
+pyautogui.press("enter")
+
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" /")
+pyautogui.press("enter")
+
+pyautogui.hotkey("ctrl" , "v" )
+pyautogui.write(" ()")
+pyautogui.press("enter")
+
